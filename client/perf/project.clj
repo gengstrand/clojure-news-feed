3916be125c92eb4e2677b89cac86f4f6b4906f29
gengstrand <glenn@dynamicalsoftware.com>
2014-09-12 00:00:18 -0700

    using hadoop 1.2.1 because I found documentation in cascalog saying that is a supported verion

--- client/perf/project.clj
+++ client/perf/project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [cascalog "2.0.0"]
-                 [org.apache.hadoop/hadoop-core "1.0.4"]
+                 [org.apache.hadoop/hadoop-core "1.2.1"]
                  [cascalog/cascalog-more-taps "2.1.1"]]
   :main ^:skip-aot perf.core
   :target-path "target/%s"
