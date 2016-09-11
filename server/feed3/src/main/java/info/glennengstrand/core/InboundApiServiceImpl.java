5fd17ee76e4cacb29d20389c003eb4de6b502a6b
Glenn Engstrand <gengstrand@gmail.com>
2016-09-10 23:53:51 -0700

    wiring up the DAO in the services

--- server/feed3/src/main/java/info/glennengstrand/core/InboundApiServiceImpl.java
+++ server/feed3/src/main/java/info/glennengstrand/core/InboundApiServiceImpl.java
@@ -2,15 +2,24 @@ package info.glennengstrand.core;
 
 import java.util.List;
 
+import com.google.inject.Inject;
+
 import info.glennengstrand.api.Inbound;
+import info.glennengstrand.db.InboundDAO;
 import info.glennengstrand.resources.InboundApi.InboundApiService;
 
 public class InboundApiServiceImpl implements InboundApiService {
 
+	private final InboundDAO dao;
+	
 	@Override
 	public List<Inbound> getInbound(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		return dao.fetch(id);
+	}
+	
+	@Inject
+	public InboundApiServiceImpl(InboundDAO dao) {
+		this.dao = dao;
 	}
 
 }
--- server/feed3/src/main/java/info/glennengstrand/core/OutboundApiServiceImpl.java
+++ server/feed3/src/main/java/info/glennengstrand/core/OutboundApiServiceImpl.java
@@ -2,27 +2,54 @@ package info.glennengstrand.core;
 
 import java.util.List;
 
+import com.google.inject.Inject;
+
+import info.glennengstrand.api.Friend;
+import info.glennengstrand.api.Inbound;
 import info.glennengstrand.api.Outbound;
+import info.glennengstrand.db.InboundDAO;
+import info.glennengstrand.db.OutboundDAO;
+import info.glennengstrand.resources.FriendApi.FriendApiService;
 import info.glennengstrand.resources.OutboundApi.OutboundApiService;
 
 public class OutboundApiServiceImpl implements OutboundApiService {
 
+	private final OutboundDAO outdao;
+	private final InboundDAO indao;
+	private final FriendApiService friendService;
+	
 	@Override
 	public Outbound addOutbound(Outbound body) {
-		// TODO Auto-generated method stub
-		return null;
+		for (Friend friend : friendService.getFriend(body.getId())) {
+			Inbound in = new Inbound.InboundBuilder()
+					.withFrom(body.getId())
+					.withTo(friend.getTo())
+					.withOccurred(body.getOccurred())
+					.withSubject(body.getSubject())
+					.withStory(body.getStory())
+					.build();
+			indao.create(in);
+		}
+		outdao.create(body);
+		return body;
 	}
 
 	@Override
 	public List<Outbound> getOutbound(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		return outdao.fetch(id);
 	}
 
 	@Override
 	public List<Outbound> searchOutbound(String keywords) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	
+	@Inject
+	public OutboundApiServiceImpl(OutboundDAO outdao, InboundDAO indao, FriendApiService friendService) {
+		this.outdao = outdao;
+		this.indao = indao;
+		this.friendService = friendService;
 	}
 
 }
