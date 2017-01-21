a86b358742ebaab3a7e2647ec8d6f22aed2a3568
Glenn Engstrand <gengstrand@gmail.com>
2017-01-21 12:19:46 -0800

    last minute changes to get this to work in my next load test

--- server/feed4/index.js
+++ server/feed4/index.js
@@ -11,15 +11,15 @@ var serverPort = 8080;
 const numCPUs = require('os').cpus().length;
 
 if (cluster.isMaster) {
-  console.log(`Master ${process.pid} is running`);
+  console.log('Master ${process.pid} is running');
 
   // Fork workers.
   for (let i = 0; i < numCPUs; i++) {
     cluster.fork();
   }
 
-  cluster.on('exit', (worker, code, signal) => {
-    console.log(`worker ${worker.process.pid} died`);
+  cluster.on('exit', function(worker, code, signal) {
+    console.log('worker ${worker.process.pid} died');
   });
 } else {
   // swaggerRouter configuration
