a09fcf9a66ebfa9ef9edbd0d850ac2283ed804bf
Glenn Engstrand <gengstrand@gmail.com>
2017-01-20 22:58:29 -0800

    unit tests for perf5 and cluster mode for feed4

--- client/perf5/test/perf5/core_test.clj
+++ client/perf5/test/perf5/core_test.clj
@@ -1,7 +1,44 @@
 (ns perf5.core-test
   (:require [clojure.test :refer :all]
-            [perf5.core :refer :all]))
+            [perf5.core :refer :all]
+            [clojure.data.json :as json]))
 
-(deftest a-test
+(def searcher-stats-numdocs 23)
+(def searcher-stats-numdocs-json
+  (str 
+    "{\"searcher\":{\"stats\":{\"numDocs\":"
+    searcher-stats-numdocs
+    "}}}"))
+  
+(deftest parse-int-test
   (testing "FIXME, I fail."
-    (is (= 0 1))))
+    (is (= 5 (parse-int "5")))))
+
+(deftest extract-num-docs-test
+  (extract-num-docs (json/read-str searcher-stats-numdocs-json))
+  (testing "extract numDocs from json"
+     (is 
+       (=
+         searcher-stats-numdocs
+         total-docs))))
+
+(deftest refresh-stats-test 
+  (refresh-stats 5 6 7 8)
+  (let [s (refresh-stats 6 7 8 9)]
+    (testing "keep single interval stats from running total stats"
+      (is 
+        (and (= (:index-total s) 1)
+             (= (:index-time s) 1)
+             (= (:query-total s) 1)
+             (= (:query-time s) 1))))))
+
+(def extract-result 1)
+
+(defn set-extract-result 
+  [value]
+  (def extract-result value))
+
+(deftest following-test 
+  (following ["miss" 0 "hit" 2] "hit" set-extract-result)
+  (testing "extract result following tag"
+     (is (= extract-result 2))))
