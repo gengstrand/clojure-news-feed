0c1c4f803b3888ab41ed00bda9526894111e0a1a
Glenn Engstrand <gengstrand@gmail.com>
2017-08-10 23:59:32 -0700

    We don't need kube-dns at all now.

--- server/k8s/setup.sh
+++ server/k8s/setup.sh
@@ -1,11 +1,8 @@
-kubectl create -f https://github.com/kubernetes/minikube/blob/master/deploy/addons/kube-dns/kube-dns-svc.yaml
 kubectl create -f cassandra-service.yaml
 kubectl create -f redis-service.yaml
 kubectl create -f mysql-service.yaml
 kubectl create -f elasticsearch-service.yaml
 sleep 10
-kubectl create -f https://github.com/kubernetes/minikube/blob/master/deploy/addons/kube-dns/kube-dns-controller.yaml
-kubectl create -f https://github.com/kubernetes/minikube/blob/master/deploy/addons/kube-dns/kube-dns-cm.yaml
 kubectl create -f cassandra-deployment.yaml
 kubectl create -f redis-deployment.yaml
 kubectl create -f mysql-deployment.yaml
