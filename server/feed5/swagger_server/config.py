a706f1676691489c23304202889cc2f1fda1acaa
Glenn Engstrand <gengstrand@gmail.com>
2017-08-26 10:08:40 -0700

    performance tuning feed 5 on aws

--- server/feed5/swagger_server/config.py
+++ server/feed5/swagger_server/config.py
@@ -1,5 +1,6 @@
 class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_POOL_SIZE = 18
     CACHE_PORT = 6379
     NOSQL_KEYSPACE = 'activity'
     NOSQL_CONSISTENCY_LEVEL = 'one'
