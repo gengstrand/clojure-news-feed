bbb40cde4594e5354e13c224171612474dd96a60
Glenn Engstrand <media@glennengstrand.info>
2018-03-25 00:51:03 -0700

    we were able to switch back to openjdk once we upgraded to newer versions of clojure and ring

---
+++ server/feed/config.clj
@@ -0,0 +1,12 @@
+{:sql-host "mysql"
+ :sql-db-name "feed"
+ :sql-db-user "feed"
+ :sql-db-password "feed1234"
+ :sql-vendor "mysql"
+ :nosql-host "cassandra"
+ :nosql-consistency "one" 
+ :nosql-ttl 7776000 
+ :cache-host "redis"
+ :messaging-host "127.0.0.1"
+ :search-host "http://elasticsearch:9200/feed/stories"}
+
