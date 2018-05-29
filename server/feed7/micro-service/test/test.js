7e2cb69b29f9548a49a960f86a42d32c2b976325
Glenn Engstrand <media@glennengstrand.info>
2018-05-28 23:56:28 -0700

    unit tests working and all passed

--- server/feed7/micro-service/test/test.js
+++ server/feed7/micro-service/test/test.js
@@ -1,171 +1,89 @@
 const expect = require('chai').expect;
 const mockery = require('mockery');
-const args = {'id': {'value': 1}};
-const composerParticipantMock = {
-	BusinessNetworkConnection: function() {
-	};
-	BusinessNetworkConnection.prototype.connect = function(cardName) {
-	    return new Promise(function(resolve) {
-		const definition = {
-		    getFactory: function() {
-			return {
-			};
-		    }
-		};
-		resolve(definition);
-	    });
-	};
-	BusinessNetworkConnection.prototype.getParticipantRegistry = function(name) {
-	    return new Promise(function(resolve) {
-		const registry = {
-		    get: function(key) {
-			return new Promise(function(resolve) {
-			    const result = {
-				moniker: 'smith'
-			    };
-			    resolve(result);
-			};
-		    }
-		};
-		resolve(registry);
-	    };
-	};
+
+function BusinessNetworkConnection() {
 }
-const composerFriendsMock = {
-    BusinessNetworkConnection: {
-	function BusinessNetworkConnection() {
-	},
-	BusinessNetworkConnection.prototype.connect = function(cardName) {
-	    return new Promise(function(resolve) {
-		const definition = {
-		    getFactory: function() {
+BusinessNetworkConnection.prototype.connect = function(cardName) {
+    return new Promise(function(resolve) {
+	const definition = {
+	    getFactory: function() {
+		return {
+		    newTransaction: function(ns, t) {
 			return {
+			    sender: '',
+			    subject: '',
+			    story: ''
 			};
+		    },
+		    newRelationship: function(ns, t, id) {
+			return ns + '.' + t + '#' + id;
 		    }
 		};
-		resolve(definition);
-	    });
-	},
-	BusinessNetworkConnection.prototype.getParticipantRegistry = function(name) {
-	    return new Promise(function(resolve) {
-		const registry = {
-		    get: function(key) {
-			return new Promise(function(resolve) {
-			    const result = [
-				{
-				    participantId: 3
-				}
-			    ];
-			    resolve(result);
-			};
-		    }
-		};
-		resolve(registry);
-	    };
-	},
-	BusinessNetworkConnection.prototype.buildQuery = function(sql) {
-	    return null;
-	},
-	BusinessNetworkConnection.prototype.query = function(query, params) {
-	    return new Promise(function(resolve) {
-		const results = {
-		};
-		resolve(results);
 	    }
-	}
-    }
-}
-const composerInbouundMock = {
-    BusinessNetworkConnection: {
-	function BusinessNetworkConnection() {
-	},
-	BusinessNetworkConnection.prototype.connect = function(cardName) {
-	    return new Promise(function(resolve) {
-		const definition = {
-		    getFactory: function() {
-			return {
-			};
-		    }
-		};
-		resolve(definition);
-	    });
-	},
-	BusinessNetworkConnection.prototype.getParticipantRegistry = function(name) {
-	    return new Promise(function(resolve) {
-		const registry = {
-		    get: function(key) {
-		    }
-		};
-		resolve(registry);
-	    };
-	},
-	BusinessNetworkConnection.prototype.buildQuery = function(sql) {
-	    return null;
-	},
-	BusinessNetworkConnection.prototype.query = function(query, params) {
-	    return new Promise(function(resolve) {
-		const results = [
-		    {
-			created: Date.now(),
-			subject: 'test',
-			story: 'test'
-		    }
-		];
-		resolve(results);
-	    }
-	}
-    }
-}
-const composerOutboundMock = {
-    BusinessNetworkConnection: {
-	function BusinessNetworkConnection() {
-	},
-	BusinessNetworkConnection.prototype.connect = function(cardName) {
-	    return new Promise(function(resolve) {
-		const definition = {
-		    getFactory: function() {
-			return {
-			    newTransaction: function(ns, t) {
-				return {
-				    subject: '',
-				    story: '',
-				    sender: ''
-				};
-			    },
-			    newRelationship: function(ns, t, id) {
-				return null;
+	};
+	resolve(definition);
+    });
+};
+BusinessNetworkConnection.prototype.getParticipantRegistry = function(name) {
+    return new Promise(function(resolve) {
+	const registry = {
+	    get: function(key) {
+		return new Promise(function(resolve) {
+		    const result = {
+			moniker: 'smith',
+			friends: [
+			    {
+				participantId: 3
 			    }
-			};
-		    }
-		};
-		resolve(definition);
-	    });
-	},
-	BusinessNetworkConnection.prototype.submitTransaction = function(t) {
-	    return new Promise(function(resolve) {
-		resolve(null);
-	    };
-	},
-	BusinessNetworkConnection.prototype.buildQuery = function(sql) {
-	    return null;
-	},
-	BusinessNetworkConnection.prototype.query = function(query, params) {
-	    return new Promise(function(resolve) {
-		const results = {
-		};
-		resolve(results);
+			]
+		    };
+		    resolve(result);
+		});
 	    }
+	};
+	resolve(registry);
+    });
+}
+BusinessNetworkConnection.prototype.buildQuery = function(sql) {
+	    return 'SELECT info.glennengstrand.Inbound WHERE (recipient == _$broadcaster)';
+},
+BusinessNetworkConnection.prototype.query = function(query, params) {
+    return new Promise(function(resolve) {
+	const results = [
+	{
+	    participantId: 3, 
+	    created: Date.now(),
+	    subject: 'test',
+	    story: 'test'
 	}
+	];
+	resolve(results);
+    });
+};
+BusinessNetworkConnection.prototype.submitTransaction = function(t) {
+	return new Promise(function(resolve) {
+	    const result = {
+	    };
+	    resolve(result);
+	});
+};
+const composerMock = {
+    BusinessNetworkConnection
+}
+const elasticMock = {
+    index: function(from, story) {
     }
 }
 describe('participant service', function() {
     before(function() {
 	mockery.enable();
 	mockery.registerAllowable('composer-client');
-	mockery.registerMock('composer-client', composerParticipantMock);
+	mockery.registerAllowable('../services/ParticipantService');
+	mockery.registerMock('composer-client', composerMock);
     });
     it('fetches a participant', function() {
 	const participantService = require('../services/ParticipantService');
+	const args = {'id': {'value': 1}};
 	participantService.getParticipant(args, function(err, participant) {
 	    expect(err).to.equal(null);
 	    expect(participant.id).to.equal(1);
@@ -180,14 +98,16 @@ describe('friends service', function() {
     before(function() {
 	mockery.enable();
 	mockery.registerAllowable('composer-client');
-	mockery.registerMock('composer-client', composerFriendsMock);
+	mockery.registerAllowable('../services/FriendService');
+	mockery.registerMock('composer-client', composerMock);
     });
     it('fetches participant friends', function() {
 	const service = require('../services/FriendService');
+	const args = {'id': {'value': 2}};
 	service.getFriend(args, function(err, friend) {
 	    expect(err).to.equal(null);
-	    expect(friend.from).to.equal(1);
-	    expect(friend.to).to.equal(3);
+	    expect(friend[0].from).to.equal(2);
+	    expect(friend[0].to).to.equal(3);
 	});
     });
     after(function() {
@@ -198,16 +118,16 @@ describe('inbound service', function() {
     before(function() {
 	mockery.enable();
 	mockery.registerAllowable('composer-client');
-	mockery.registerMock('composer-client', composerInboundMock);
+	mockery.registerAllowable('../services/InboundService');
+	mockery.registerMock('composer-client', composerMock);
     });
     it('fetches inbound feed', function() {
 	const args = {'id': {'value': 1}};
 	const inboundService = require('../services/InboundService');
 	inboundService.getInbound(args, function(err, feed) {
 	    expect(err).to.equal(null);
-	    expect(feed.to).to.equal(3);
-	    expect(feed.occurred).to.equal(Date.now());
-	    expect(feed.subject).to.equal('test');
+	    expect(feed[0].to).to.equal(1);
+	    expect(feed[0].subject).to.equal('test');
 	});
     });
     after(function() {
@@ -218,8 +138,10 @@ describe('outbound service', function() {
     before(function() {
 	mockery.enable();
 	mockery.registerAllowable('composer-client');
+	mockery.registerAllowable('../services/OutboundService');
 	mockery.registerAllowable('../repositories/elastic');
-	mockery.registerMock('composer-client', composerOutboundMock);
+	mockery.registerMock('composer-client', composerMock);
+	mockery.registerMock('../repositories/elastic', elasticMock);
     });
     it('saves outbound feed', function() {
 	const postArgs = {
@@ -232,7 +154,7 @@ describe('outbound service', function() {
 	const outboundService = require('../services/OutboundService');
 	outboundService.addOutbound(postArgs, function(err, feed) {
 	    expect(err).to.equal(null);
-	    expect(feed.from).to.equal(1);
+	    expect(feed.from).to.equal('1');
 	    expect(feed.occurred).to.equal(Date.now());
 	    expect(feed.subject).to.equal('test');
 	});
