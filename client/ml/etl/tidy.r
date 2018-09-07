ab02c63002601fff454c083138f4038faef1190c
gengstrand <gengstrand@gmail.com>
2018-09-07 08:33:32 -0700

    not sure why but pipes in R are not working

--- client/ml/etl/tidy.r
+++ client/ml/etl/tidy.r
@@ -7,12 +7,12 @@ if (is.na(args[1]) || is.na(args[2])) {
 print(paste("reading from ", args[1]))
 print(paste("writing to ", args[2]))
 raw <- read_csv(args[1])
-refined <- select(raw, year, month, day, hour, minute, second, cloud, feed, entity, operation, status, duration) %>%
-filter(status == 200) %>%
-mutate(entityoperation = paste(entity, operation)) %>%
-group_by(year, month, day, hour, minute, second, cloud, feed, entityoperation) %>%
-summarise(duration = mean(duration)) %>%
-spread(key = entityoperation, value = duration) %>%
-refined[is.na(refined)] <- 0
-write_csv(refined, path=args[2])
+r1 <- select(raw, year, month, day, hour, minute, second, cloud, feed, entity, operation, status, duration) 
+r2 <- filter(r1, status == 200) 
+r3 <- mutate(r2, entityoperation = paste(entity, operation))
+r4 <- group_by(r3, year, month, day, hour, minute, second, cloud, feed, entityoperation)
+r5 <- summarise(r4, duration = mean(duration)) 
+r6 <- spread(r5, key = entityoperation, value = duration) 
+r6[is.na(r6)] <- 0
+write_csv(r6, path=args[2])
 
