2b0dafd4b0312c5d5d23efec20de765249a0d8b1
gengstrand <gengstrand@gmail.com>
2018-12-29 10:28:05 -0800

    investigating a big elastic search upgrade

--- server/feed/etc/initCluster.sh
+++ server/feed/etc/initCluster.sh
@@ -6,16 +6,16 @@ EOF
 sleep 10
 mysql -h mysql -u feed -pfeed1234 --database=feed <schema.mysql.sql
 cqlsh cassandra <schema.cassandra.sql
-curl -XPOST http://elasticsearch:9200/feed -d '{
+curl -XPUT -H 'Content-Type: application/json' http://elasticsearch/feed -d '{
     "settings" : {
         "number_of_shards" : 1
     },
     "mappings" : {
         "stories" : {
             "properties" : {
-                "id" : { "type" : "string", "index" : "not_analyzed" },
-                "sender" : { "type" : "string", "index" : "not_analyzed" },
-                "story" : { "type" : "string"}
+                "id" : { "type" : "keyword" },
+                "sender" : { "type" : "keyword" },
+                "story" : { "type" : "text"}
             }
         }
     }
