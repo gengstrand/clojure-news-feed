e15ff25eb0179abceb0f61b6b6b1848ca43b9852
gengstrand <gengstrand@gmail.com>
2021-04-18 13:19:45 -0700

    mysql connection pooling for feed 13

--- server/feed13/src/feed/daos/relational.clj
+++ server/feed13/src/feed/daos/relational.clj
@@ -1,5 +1,6 @@
 (ns feed.daos.relational
-  (:import (org.jdbi.v3.core Jdbi)))
+  (:import (org.jdbi.v3.core Jdbi)
+           (java.util Properties)))
 
 (def jdbi (atom "jdbc:h2:mem:feed;DB_CLOSE_DELAY=-1"))
 
@@ -9,6 +10,10 @@
   (let [host (or (System/getenv "MYSQL_HOST") "mysql")
         db (or (System/getenv "MYSQL_DB") "feed")
         user (or (System/getenv "MYSQL_USR") "feed")
-        password (or (System/getenv "MYSQL_PWD") "feed1234")]
-        (swap! jdbi (fn [cs] (Jdbi/create (str "jdbc:mysql://" host ":3306/" db) user password)))))
-
+        password (or (System/getenv "MYSQL_PWD") "feed1234")
+        props (Properties.)]
+        (.setProperty props "user" user)
+        (.setProperty props "password" password)
+        (.setProperty props "minSize" "3")
+        (.setProperty props "maxSize" "18")
+        (swap! jdbi (fn [cs] (Jdbi/create (str "jdbc:mysql://" host ":3306/" db) props)))))
