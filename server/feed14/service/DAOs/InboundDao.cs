c31e473c322bdd2ceb7a38c98c61f68deb4d8de6
gengstrand <gengstrand@gmail.com>
2023-10-22 12:44:19 -0700

    inbound integration with cassandra and integration with elasticsearch

--- server/feed14/service/DAOs/InboundDao.cs
+++ server/feed14/service/DAOs/InboundDao.cs
@@ -1,20 +1,57 @@
 using Microsoft.AspNetCore.Mvc;
 using newsfeed.Interfaces;
 using newsfeed.Models;
+using Cassandra;
 
 namespace newsfeed.DAOs;
 
-public class InboundDao : IInboundDao
+public class InboundDao : CassandraDao, IInboundDao
 {
     static readonly ILogger<InboundDao> logger = new LoggerFactory().CreateLogger<InboundDao>();
+    private PreparedStatement? upsert;
+    private PreparedStatement? select;
+
+    private PreparedStatement Upsert {
+        get {
+            if (upsert == null) {
+                lock (logger) {
+                    if (upsert == null) {
+                        upsert = CassandraSession.Prepare("insert into Inbound (ParticipantID, FromParticipantID, Occurred, Subject, Story) values (?, ?, now(), ?, ?) using ttl");
+                    }
+                }
+            }
+            return upsert;
+        }
+    }
+
+    private PreparedStatement Select {
+        get {
+            if (select == null) {
+                lock (logger) {
+                    if (select == null) {
+                        select = CassandraSession.Prepare("select Occurred, FromParticipantID, Subject, Story from Inbound where ParticipantID = ? order by Occurred desc");
+                    }
+                }
+            }
+            return select;
+        }
+    }
 
     public async Task<Outbound> CreateInboundAsync(string id, Inbound inbound)
     {
-        throw new NotImplementedException();
+        var s = Upsert.Bind(inbound.To, id, inbound.Subject, inbound.Story);
+        var rs = await CassandraSession.ExecuteAsync(s);
+        return new Inbound(id, inbound.To, new DateOnly(), inbound.Subject, inbound.Story);
     }
 
     public async Task<IEnumerable<Inbound>> GetInboundAsync(string id)
     {
-        throw new NotImplementedException();
+        var s = Select.Bind(id);
+        var rs = await CassandraSession.ExecuteAsync(s);
+        List<Inbound> rv = new();
+        foreach (var row in rs) {
+            rv.Add(new Inbound(row.GetValue<string>("FromParticipantID"), id, row.GetValue<DateOnly>("Occurred"), row.GetValue<string>("Subject"), row.GetValue<string>("Story")));
+        }
+        return rv;
     }
 }
--- server/feed14/service/DAOs/SearchDao.cs
+++ server/feed14/service/DAOs/SearchDao.cs
@@ -1,20 +1,64 @@
 using Microsoft.AspNetCore.Mvc;
 using newsfeed.Interfaces;
+using System.Text.Json;
 using newsfeed.Models;
 
 namespace newsfeed.DAOs;
 
 public class SearchDao : ISearchDao
 {
+    class SearchDocument {
+        public string Id { get; }
+        public long Sender { get; }
+        public string Story { get; }
+        public SearchDocument(string id, long sender, string story) {
+            Id = id;
+            Sender = sender;
+            Story = story;
+        }
+    
+    }
     static readonly ILogger<SearchDao> logger = new LoggerFactory().CreateLogger<SearchDao>();
+
+    private static readonly HttpClient client = new() {
+        BaseAddress = new Uri($"http://{Environment.GetEnvironmentVariable("SEARCH_HOST") ?? "elasticsearch"}"),
+    };
 
     public async Task<bool> IndexAsync(string id, string sender, string story)
     {
-        throw new NotImplementedException();
+        SearchDocument s = new(id, long.Parse(sender), story);
+        JsonContent request = JsonContent.Create<SearchDocument>(s);
+        var r = await client.PutAsync($"/feed/stories/{id}", request);
+        return r.IsSuccessStatusCode;
     }
 
     public async Task<IEnumerable<string>> SearchAsync(string keywords)
     {
-        throw new NotImplementedException();
+        List<string> rv = new();
+        var resp = await client.GetAsync($"/feed/stories/_search?q={keywords}");
+        if (resp.IsSuccessStatusCode) {
+            var d = await resp.Content.ReadAsStringAsync();
+            var jd = JsonSerializer.Deserialize<Dictionary<string, object>>(d);
+            if (jd != null && jd.ContainsKey("hits")) {
+                var hits = jd["hits"] as Dictionary<string, object>;
+                if (hits != null && hits.ContainsKey("hits")) {
+                    var hitsList = hits["hits"] as List<Dictionary<string, object>>;
+                    if (hitsList != null) {
+                        foreach (var h in hitsList) {
+                            if (h != null && h.ContainsKey("_source")) {
+                                var s = h["_source"] as Dictionary<string, object>;
+                                if (s != null && s.ContainsKey("sender")) {
+                                    object o = s["sender"];
+                                    if (o != null) {
+                                        rv.Add(o.ToString());
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return rv;
     }
 }
